bdc9b22a726f10bb3858b65f12dd49440c541e66
Radu Stancu <radustancu@Radus-MacBook-Air.local>
2023-05-26 09:30:50 +0300

    server

--- PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Calculus.java
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Calculus.java
@@ -4,20 +4,24 @@ import androidx.annotation.NonNull;
 
 public class Calculus {
 
-    private final Integer add;
-    private final Integer mul;
+    //private final Integer add;
+    //private final Integer mul;
+    private final Integer operator_1;
+    private final Integer operator_2;
 
-    public Calculus(Integer add, Integer mul) {
-        this.add = add;
-        this.mul = mul;
+    public Calculus(Integer add, Integer mul, Integer operator_1, Integer operator_2) {
+        //this.add = add;
+        //this.mul = mul;
+        this.operator_1 = operator_1;
+        this.operator_2 = operator_2;
     }
 
     public Integer getAdd() {
-        return add;
+        return operator_1 + operator_2;
     }
 
     public Integer getMul() {
-        return mul;
+        return operator_1 * operator_2;
     }
 
 }
