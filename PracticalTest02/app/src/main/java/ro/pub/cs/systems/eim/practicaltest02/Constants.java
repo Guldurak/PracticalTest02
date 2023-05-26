6b1e8a5d944cec2960ed3c4b31fed96c1b2696ed
Radu Stancu <radustancu@Radus-MacBook-Air.local>
2023-05-26 08:27:29 +0300

    interfata grafica

--- PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     final public static String TAG = "[PracticalTest02]";
     final public static boolean DEBUG = true;
 
-    final public static String WEB_SERVICE_API_KEY = "dbd3b02d8958d62185d02e944cd5f522";
+    final public static String WEB_SERVICE_API_KEY = "e03c3b32cfb5a6f7069f2ef29237d87e";
     final public static String WEB_SERVICE_ADDRESS = "http://api.openweathermap.org/data/2.5/weather";
 
     final public static String UNITS = "metric";
